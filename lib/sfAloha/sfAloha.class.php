800240367e8831d5aeb3854f5ee6bc687714a2b0
Michaël Perrin <michael@perrin.se>
2012-06-05 17:22:06 +0200

    Add option to allow content edit only for users who are authenticated or who have specific credentials

--- lib/sfAloha/sfAloha.class.php
+++ lib/sfAloha/sfAloha.class.php
@@ -72,4 +72,48 @@ class sfAloha
   {
     return self::$_backend;
   }
+
+  /**
+   * Checks if the current user has rights to edit content
+   *
+   * @return boolean false if user doesn't have rights, true otherwise
+   */
+  public function checkAccess()
+  {
+    $user = sfContext::getInstance()->getUser();
+
+    $securityConf = sfConfig::get('app_aloha_security');
+
+    // If no configuration is found regarding security, do not allow any action
+    if (empty($securityConf) || !isset($securityConf['edit']))
+    {
+      return false;
+    }
+
+    $editSecurity = $securityConf['edit'];
+
+    // Check authentication
+    if (!array_key_exists('authenticated', $editSecurity))
+    {
+      return false;
+    }
+
+    if ($editSecurity['authenticated'] != false && !$user->isAuthenticated())
+    {
+      return false;
+    }
+
+    // Check credentials
+    if (!array_key_exists('credentials', $editSecurity))
+    {
+      return false;
+    }
+
+    if ($editSecurity['credentials'] != false && !$user->hasCredential($editSecurity['credentials']))
+    {
+      return false;
+    }
+
+    return true;
+  }
 }
